f57792e5f922d1ca61ac07e97def59db486f3b12
longpd <longpddev@gmail.com>
2022-10-03 21:06:05 +0700

    add card animate config

--- src/modules/users/dto/settings.user.dto.ts
+++ src/modules/users/dto/settings.user.dto.ts
@@ -1,10 +1,15 @@
-import { IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
+type CardAnimate = 'fade' | 'slide' | 'none';
 export class SettingsUser {
   @IsNumber()
   maxCardInDay: number;
+
+  @IsNotEmpty()
+  cardAnimate: CardAnimate;
 }
 
 export const defaultSetting = new SettingsUser();
 
 defaultSetting.maxCardInDay = 30;
+defaultSetting.cardAnimate = 'fade';
